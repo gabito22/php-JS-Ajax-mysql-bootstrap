fb57d4cc5bfec5864ae1fc33036bb331d976a857
gabriel <el.gabooooo@live.com>
2018-09-12 00:28:44 -0300

    primer commit

---
+++ admin/footer.php
@@ -0,0 +1,5 @@
+<footer class="footer">
+      <div class="container">
+        <p class="text-muted">Desarrollado por <a href="https://web.facebook.com/gabrielmarcelorojaspizarro/" target='_blank'>Gabriel Rojas</a></p>
+      </div>
+</footer>
